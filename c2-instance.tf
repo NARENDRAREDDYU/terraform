79408d518fc6a3cf58c70b6dd69c2d13362f2ae7
NARENDRAREDDYU <108210882+NARENDRAREDDYU@users.noreply.github.com>
2022-09-01 10:47:04 +0530

    security group association added

--- c2-instance.tf
+++ c2-instance.tf
@@ -23,6 +23,7 @@ resource "aws_instance" "ec2_instnce" {
     instance_type = var.aws_instance
     key_name = var.key_name
     user_data = file ("${path.module}/app1-install.sh")
+    vpc_security_group_ids = [aws_security_group.vpc-ssh.id , aws_security_group.vpc-web.id]
     tags = {
       "Name" = "ec2_instnace"
     }
